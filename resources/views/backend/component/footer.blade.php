cde94c2b47037b956b5d6f174977320b870bf682
Ahmad Krisdiantoro <achmadcrizdiantho10@gmail.com>
2023-07-25 21:48:17 +0700

    first

---
+++ resources/views/backend/component/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>SISDES PAGERGUNUNG</span>
+            <!-- {{-- <span>Copyright {{date('Y')}} digsa.id. All Right Reserved</span> --}} -->
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
